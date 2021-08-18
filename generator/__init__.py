b387b9a9cec89210138320ef16cba27a115a730c
Matt-Klaus Dev (Lev) <develper.mattklaus@gmail.com>
2021-08-18 20:53:53 +0200

    fix download link 5

--- generator/__init__.py
+++ generator/__init__.py
@@ -7,7 +7,7 @@ def create_app(environ=None, start_response=None, test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'generator.sqlite'),
-        UPLOAD_FOLDER=os.path.join(app.instance_path, '../instance/uploads'),
+        UPLOAD_FOLDER=os.path.join(app.instance_path, '../../instance/uploads'),
         GENERATED_DIR='generated',
         LOG_FILENAME='output.log',
         FILE_STATUSES={'Undone': -1, 'Ongoing': 0, 'Done': 1}
